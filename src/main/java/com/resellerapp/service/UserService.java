6af46ac62a4bd841b305e8f370a90717c2fdec98
Simovanev <simovanev@abv.bg>
2024-10-21 06:25:57 +0300

    Home page and login

--- src/main/java/com/resellerapp/service/UserService.java
+++ src/main/java/com/resellerapp/service/UserService.java
@@ -11,6 +11,10 @@ import java.util.Optional;
 public class UserService {
     private UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public boolean isLogged(LoginDto loginDto) {
         Optional<User> user = userRepository.findByUsernameAndPassword(loginDto.getUsername(), loginDto.getPassword());
         return user.isPresent();
