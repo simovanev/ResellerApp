6bffc1b3a033dd383df241e8e5a21d493dd5540c
PC1237 <simovanev@abv.bg>
2024-10-23 15:08:36 +0300

    Register form with validations

--- src/main/java/com/resellerapp/controller/HomeController.java
+++ src/main/java/com/resellerapp/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.resellerapp.controller;
 
 import com.resellerapp.session.CurrentUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.Column;
@@ -10,11 +11,21 @@ import javax.persistence.Column;
 public class HomeController {
     private CurrentUser currentUser;
 
+    public HomeController(CurrentUser currentUser) {
+        this.currentUser = currentUser;
+    }
+
     @GetMapping("/home")
-    public String home() {
+    public String home( Model model) {
         if (currentUser.getId() == 0) {
             return "redirect:/";
         }
+        model.addAttribute("userName", currentUser);
+
+
+
+
+
         return "home";
     }
 }
